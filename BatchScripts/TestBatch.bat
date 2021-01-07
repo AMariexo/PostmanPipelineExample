d2734aeb1f5597b120beb058097849734a55d0ed
AMariexo <alymarie561@gmail.com>
2021-01-06 23:16:30 -0500

    fixing variable

--- BatchScripts/TestBatch.bat
+++ BatchScripts/TestBatch.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-newman run Collections\Practice.postman_collection.json -e Environments\%environ%.json --reporters cli,junit --reporter-junit-export Results\junitReport.xml
+newman run Collections\Practice.postman_collection.json -e Environments\%1.json --reporters cli,junit --reporter-junit-export Results\junitReport.xml
 PAUSE
