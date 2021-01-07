18aac796b298d679ccfc84db0b9531ad20a30cd8
AMariexo <alymarie561@gmail.com>
2021-01-06 22:57:44 -0500

    Fixing pipeline failure with variable

--- BatchScripts/TestBatch.bat
+++ BatchScripts/TestBatch.bat
@@ -1,4 +1,3 @@
 @ECHO OFF
-set environ = %1
 newman run Collections\Practice.postman_collection.json -e Environments\%environ%.json --reporters cli,junit --reporter-junit-export Results\junitReport.xml
 PAUSE
