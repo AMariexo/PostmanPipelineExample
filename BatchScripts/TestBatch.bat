f51338fda833d46bafde42498525bd9909c479c8
AMariexo <alymarie561@gmail.com>
2021-01-06 20:28:46 -0500

    added.json

--- BatchScripts/TestBatch.bat
+++ BatchScripts/TestBatch.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 set environ = %1
-newman run Collections\Practice.postman_collection.json -e Environments\%environ% --reporters cli,junit --reporter-junit-export Results\junitReport.xml
+newman run Collections\Practice.postman_collection.json -e Environments\%environ%.json --reporters cli,junit --reporter-junit-export Results\junitReport.xml
 PAUSE
